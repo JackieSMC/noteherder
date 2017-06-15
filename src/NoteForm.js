21ba2b37db4f9018960906bc8ad63fd5137d7a79
Jackie Amador <jamador01@saintmarys.edu>
2017-06-15 16:04:35 -0400

    Save a note from the form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,6 +4,31 @@ import './NoteForm.css'
 
 
 class NoteForm extends Component {
+    constructor(props) {
+        super(props)
+       this.state = {
+           note: this.blankNote()
+       }
+    }
+
+    blankNote = () => {
+        return {
+            id: null,
+            title: '',
+            body:'',
+
+        }
+    }
+    handleChanges = (ev) => {
+        const note = {...this.state.note}
+        note[ev.target.name] = ev.target.value
+        this.setState({ note }, ()=> this.props.saveNote(this.state.note)
+        )
+        //console.log(ev.target.name)
+        
+    }//write methods with arrow function much more future
+
+
     render() {
         return (
         <div className="NoteForm">
@@ -12,13 +37,17 @@ class NoteForm extends Component {
                     <input 
                     type="text" 
                     name="title" 
-                    placeholder="Title your note"  />
+                    placeholder="Title your note" 
+                    onChange={this.handleChanges}
+                    value={this.state.note.title} />
                 </p>
                 <p>
                     <textarea 
                     name="body" 
                     cols="30" rows="10" 
-                    placeholder="Just start typing..."></textarea>
+                    placeholder="Just start typing..."
+                    onChange={this.handleChanges}
+                    value={this.state.note.body}></textarea>
                 </p>
             </form>
         </div>
