362452cfe8df806acdd320293fb46539b175346b
Jackie Amador <jamador01@saintmarys.edu>
2017-06-15 14:36:50 -0400

    Add Note Component and render (hardcoded) notes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import Main from './Main'
 
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      notes: {
+        'note-1': {
+          id: 'note-1',
+          title: 'Thoughts on React',
+          body: 'Reach is pretty niffty. Declarative FTW!',
+        },
+        'note-2':{
+          id: 'note-2',
+          title: 'Thoughts on Reactddddd',
+          body: 'ReacdddddddddddDeclarative FTW!',
+        },
+      },
+    }
+  }
+
   render() {
     return (
       <div className="App">
 
-        <Main />
+        <Main notes={this.state.notes} />
         
       </div>
     );
