133a6696cd3776a9396d4672d348fa49d4311fa0
Jackie Amador <jamador01@saintmarys.edu>
2017-06-14 23:46:00 -0400

    Add Content and Style to Sidebar

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import './NoteForm.css'
+
+
 class NoteForm extends Component {
     render() {
         return <p>Note Form!</p>
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './NoteList.css'
+
 const NoteList = () => {
     return <p>Note List! </p>
 }
