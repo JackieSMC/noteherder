27872277105830b64c904007313588d2e5df3e5e
Jackie Amador <jamador01@saintmarys.edu>
2017-06-23 04:08:17 -0400

    HW11 Make the Page look Prettier complete. Added css across everywhere - take a look

--- src/SignIn.js
+++ src/SignIn.js
@@ -9,14 +9,27 @@ const SignIn = () => {
     
 
   return (
-    <div>
-      <button className="SignIn" onClick={()=> authenticate(githubProvider)} >
-        Sign In With Github
-      </button>  
-      <button className = "SignIn" onClick={()=> authenticate(googleProvider)}>
-        Sign in with Google
-      </button>
+    <div className="outterDiv">
+      <div className="header">
+        <h1>Notes to Herd.</h1>
+      </div>
+      <div className="secondaryheading">
+        <div className="theheading">
+        <h3> This tiger is literally me! lol. </h3>
+        <h4> Write down and enjoy the endless amount of notes! </h4>
+        </div>
+        <div className="navbar">
+          <button className="SignIn" onClick={()=> authenticate(githubProvider)} >
+            <i className="fa fa-github" aria-hidden="true"> </i> Sign In With Github
+          </button>
+          <br />  
+          <button className = "SignIn bottom" onClick={()=> authenticate(googleProvider)}>
+            <i className="fa fa-google" aria-hidden="true"></i> Sign in with Google
+          </button>
+          </div>
+        </div>
     </div>
+    
     )
 }
 
